bf760d5feafbebd6714d692996743535f6b2128b
JoaoPrimak17 <joao.primaki.souza@escola.pr.gov.br>
2026-08-19 15:10:18 +0000

    AA

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = [1111111, 22222, 33333];
+
+const informacoesPessoa = ["nome", "jose", "idade", 32, "CPF", "1111222333"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade: 32,
+    nome: "jose",
+}
