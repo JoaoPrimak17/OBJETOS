1e635b98bc5c879d2d2ff4961efe3b7cdb9306ca
JoaoPrimak17 <joao.primaki.souza@escola.pr.gov.br>
2026-08-26 15:17:21 +0000

    alterar

---
+++ alterar.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome:"Lula", 
+       profissao: "Presidiario"
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 2233444";
+
+pessoa.nome = "Lula da Silva";
+
+console.log(pessoa);
