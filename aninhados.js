112c12590b1472b9e5098c07cf48b2810a6d0326
JoaoPrimak17 <joao.primaki.souza@escola.pr.gov.br>
2026-08-27 15:21:00 +0000

    aninhados

---
+++ aninhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "João",
+    idade:23,
+    email:"Joao@firma.com",
+    telefone:["115555550", "114444440"],
+};
+
+cliente.endereco = {
+    rua: "R. da silva",
+    apartamento: true,
+    complemento: "ap 934",
+}
+
+console.log(cliente);
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "João",
+    idade:23,
+    email:"Joao@firma.com",
+    telefone:["115555550", "114444440"],
+};
+
+console.log(cliente);
